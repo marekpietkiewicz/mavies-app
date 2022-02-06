bb3d17126a71c86719e69962dcc62b922ef723e8
Marek Pietkiewicz <mmpietkiewicz@gmail.com>
2022-02-06 13:10:07 +0100

    created mandatory files

---
+++ src/pages/Dashboard/Dashboard.test.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import DashboardPage from "./DashboardPage";
+
+test("renders learn react link", () => {
+  render(<DashboardPage />);
+  const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
+});
